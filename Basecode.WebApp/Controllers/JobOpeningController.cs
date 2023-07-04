2316bde2fcbc51cb882c6b514971e6a946ccae69
Bµlawan <brianbulawan5@gmail.com>
2023-07-04 10:14:41 +0800

    File name from JobsList -> JobOpening
    Added JobOpeningController changes

--- Basecode.WebApp/Controllers/JobOpeningController.cs
+++ Basecode.WebApp/Controllers/JobOpeningController.cs
@@ -2,16 +2,25 @@ using Basecode.Data.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Basecode.WebApp.Controllers{
-    public class JobOpeningController : Controller{
-        public IActionResult Index()
-        {
-            return View();
-        }
+    public class JobOpeningController : Controller
+    {
         private readonly IJobOpeningService _service;
-
         public JobOpeningController(IJobOpeningService service)
         {
             _service = service;
+        }
+
+        public IActionResult Index()
+        {
+            var data = _service.RetrieveAll();
+            return View(data);
+        }
+
+        [HttpPost]
+        public IActionResult AddJobOpeningView(JobOpening jobOpening)
+        {
+            _service.Add(jobOpening);
+            return RedirectToAction("Index");
         }
     }
 }
