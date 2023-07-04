655e83141e563bdcf626f841581b2a537e225b4e
Bµlawan <brianbulawan5@gmail.com>
2023-07-04 10:00:54 +0800

    Update JobOpeningController.cs

--- Basecode.WebApp/Controllers/JobOpeningController.cs
+++ Basecode.WebApp/Controllers/JobOpeningController.cs
@@ -1,3 +1,17 @@
-/////////////////////////////////
-/////// Insert code here //////// 
-/////////////////////////////////
+using Basecode.Data.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Basecode.WebApp.Controllers{
+    public class JobOpeningController : Controller{
+        public IActionResult Index()
+        {
+            return View();
+        }
+        private readonly IJobOpeningService _service;
+
+        public JobOpeningController(IJobOpeningService service)
+        {
+            _service = service;
+        }
+    }
+}
