18603cf1b16641718fa5419d4603a33c3516fc85
Adrian Jay Barcenilla <111829440+adrianjaybarcenilla@users.noreply.github.com>
2023-07-04 11:46:09 +0800

    Update dbo.JobOpenings.sql

--- dbo.JobOpenings.sql
+++ dbo.JobOpenings.sql
@@ -1,14 +1,14 @@
 USE [BasecodeDB]
 GO
 
-/****** Object: Table [dbo].[JobOpenings] Script Date: 04/07/2023 8:15:48 am ******/
+/****** Object: Table [dbo].[JobOpening] Script Date: 04/07/2023 8:15:48 am ******/
 SET ANSI_NULLS ON
 GO
 
 SET QUOTED_IDENTIFIER ON
 GO
 
-CREATE TABLE [dbo].[JobOpenings] (
+CREATE TABLE [dbo].[JobOpening] (
     [Id]              INT            IDENTITY (1, 1) NOT NULL,
     [Title]           NVARCHAR (100) NOT NULL,
     [DateOfPosting]   DATE           NULL,
