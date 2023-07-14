798f318216bc329cdb51f1bee969d274bb6f2054
Bµlawan <brianbulawan5@gmail.com>
2023-07-14 13:55:22 +0800

    Revert "Revert "Login form/queen""
    This reverts commit 5bd94b3b24131bed5bad0f42e4ce32fa86030752.

---
+++ Basecode.WebApp/Controllers/LoginController.cs
@@ -0,0 +1,18 @@
+﻿using Basecode.WebApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Basecode.WebApp.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> Login([Bind("Email,Password")] LoginModel loginModel)
+        {
+            return RedirectToAction("Index", "Dashboard", loginModel);
+        }
+    }
+}
